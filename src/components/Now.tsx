4274f7ff2b19dd6bc94ae999ca2130df4c74f118
Volker Heidema <devolk015@gmail.com>
2020-10-12 22:44:27 +0200

    added use fetch hook

--- src/components/Now.tsx
+++ src/components/Now.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import moment from "moment";
 
 export const Now = () => {
-  // const lang = navigator.language;
   moment.locale("fr");
   return <span>{moment().format("DD-MMMM-YY")}</span>;
 };
