e88e8d230b420d441b3d728bad328bf47c8e7fa6
Volker Heidema <devolk015@gmail.com>
2020-10-23 23:22:05 +0200

    update fetch hook

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,14 +1,14 @@
+import { CityAirQualityData } from "model/cityAirQuality";
 import { useState, useEffect } from "react";
 
-export const useFetch = (url: string, options?: any): any => {
-  const [response, setResponse] = useState(null);
+export const useFetch = (url: string, options?: RequestInit) => {
+  const [response, setResponse] = useState<CityAirQualityData | null>(null);
   const [error, setError] = useState<string | null>(null);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
-
       try {
         const res = await fetch(url, options);
         const json = await res.json();
