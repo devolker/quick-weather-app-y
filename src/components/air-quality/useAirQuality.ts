4d86b0d1625c66e56d21e88b7b239d9196a1c3e4
Volker Heidema <devolk015@gmail.com>
2020-10-23 23:22:56 +0200

    add use air quality hook

---
+++ src/components/air-quality/useAirQuality.ts
@@ -0,0 +1,25 @@
+import { useFetch } from "hooks/useFetch";
+
+const useAirQuality = () => {
+  const airQualityApiKey = process.env.REACT_APP_AIR_QUALITY_API_KEY;
+  const latitude = 52.011578;
+  const longitude = 4.357068;
+  const { response, error, loading } = useFetch(
+    `https://api.breezometer.com/air-quality/v2/current-conditions?lat=${latitude}&lon=${longitude}&key=${airQualityApiKey}&features=breezometer_aqi,health_recommendations`
+  );
+
+  const healthRecommendations =
+    response &&
+    Object.keys(
+      response.data.health_recommendations
+    ).map((health_recommendation, index) => [
+      health_recommendation,
+      response.data.health_recommendations[index],
+    ]);
+
+  const airQualityValue = response?.data.indexes.baqi.aqi;
+
+  return { airQualityValue, error, loading, healthRecommendations };
+};
+
+export default useAirQuality;
